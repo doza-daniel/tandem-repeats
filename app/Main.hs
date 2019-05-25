ea4c13108bbe02814b625eb3c9f0b9bcc2562f69
Daniel Doza <doza.daniel@gmail.com>
2019-05-25 13:52:27 +0200

    moved suffix tree logic to lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,42 +1,10 @@
 module Main where
 
-import Data.Char
+import System.IO
 import Lib
 
 main :: IO ()
-main = someFunc
-
-data SuffixTree a = Leaf | Branch [([a], SuffixTree a)]
-type EdgeFunction a = [[a]] -> ([a], [[a]])
-
-lazyTree :: (Eq a) => (EdgeFunction a) -> [a] -> [a] -> SuffixTree a
-lazyTree edge alphabet text = sTree (suffixes text)
-    where sTree [[]] = Leaf
-          sTree ss = Branch [(a:p, sTree (y:ys)) |  a <- alphabet, (p, y:ys) <- [edge $ begginingWith a ss]]
-
-
-begginingWith a ss = [ys | y:ys <- ss, y == a]
-
-lazy_pst :: (Eq a) => [a] -> [a] -> SuffixTree a
-lazy_pst = lazyTree edge_pst
-
-edge_pst :: EdgeFunction a
-edge_pst [s] = (s, [[]])
-edge_pst ss = ([], ss)
-
-suffixes :: [a] -> [[a]]
-suffixes [] = []
-suffixes s@(x:xs) = s:suffixes xs
-
-
-
-instance (Show a) => Show (SuffixTree a) where
-    show = dropWhile isControl . pretty' 0
-
-pretty' _ Leaf = " -> Leaf"
-pretty' indent (Branch nodes) = foldl toStr "" nodes
-    where toStr acc (label, subtree) = acc ++ "\n" ++ (replicate indent '-') ++ show label ++ pretty' (indent + 2) subtree
-
-
-
+main = do
+    contents <- getContents
+    putStrLn $ pretty 0 $ buildSTree "actg$" contents
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,32 @@
 module Lib
-    ( someFunc
+    ( buildSTree
+    , pretty
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char
+
+data SuffixTree a = Leaf | Node { branches :: [([a], SuffixTree a)] } deriving (Show)
+type EdgeFunction a = [[a]] -> ([a], [[a]])
+
+lazyTree :: (EdgeFunction Char) -> String -> String -> SuffixTree Char
+lazyTree edge alphabet text = sTree (suffixes text)
+    where sTree [[]] = Leaf
+          sTree ss = Node [(a:p, sTree (y:ys)) | a <- alphabet, (p, y:ys) <- [edge $ begginingWith a ss]]
+
+begginingWith :: Char -> [String] -> [String]
+begginingWith a ss = [ys | y:ys <- ss, y == a]
+
+buildSTree ::  String -> String -> SuffixTree Char
+buildSTree = lazyTree edge_pst
+
+edge_pst :: EdgeFunction Char
+edge_pst [s] = (s, [[]])
+edge_pst ss = ([], ss)
+
+suffixes :: [a] -> [[a]]
+suffixes [] = []
+suffixes s@(x:xs) = s:suffixes xs
+
+pretty _ Leaf = " -> Leaf"
+pretty indent (Node branches) = foldl toStr "" branches
+    where toStr acc (label, subtree) = acc ++ "\n" ++ (replicate indent '-') ++ show label ++ pretty (indent + 2) subtree
