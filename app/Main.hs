8dafbba2fcf937d5f5f957ba380874079291a4c5
Daniel Doza <doza.daniel@gmail.com>
2019-05-25 14:05:28 +0200

    finding and sorting all occurrences that appear more than 5 times in a dna sequence read from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
 import System.IO
+import Data.List
 import Lib
+
+import qualified Data.Map as M
+
 
 main :: IO ()
 main = do
     contents <- getContents
-    putStrLn $ pretty 0 $ buildSTree "actg$" contents
-
+    print $ reverse . sortOn snd . M.toList . M.filter (>5) . findAllOccurences "" $ buildSTree "actg$" contents
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,11 @@
 module Lib
     ( buildSTree
+    , findAllOccurences
     , pretty
     ) where
 
 import Data.Char
+import qualified Data.Map as M
 
 data SuffixTree a = Leaf | Node { branches :: [([a], SuffixTree a)] } deriving (Show)
 type EdgeFunction a = [[a]] -> ([a], [[a]])
@@ -30,3 +32,13 @@ suffixes s@(x:xs) = s:suffixes xs
 pretty _ Leaf = " -> Leaf"
 pretty indent (Node branches) = foldl toStr "" branches
     where toStr acc (label, subtree) = acc ++ "\n" ++ (replicate indent '-') ++ show label ++ pretty (indent + 2) subtree
+
+cntL :: SuffixTree a -> Int
+cntL Leaf = 1
+cntL node = sum $ map (cntL . snd) $ branches node
+
+findAllOccurences :: String -> SuffixTree Char -> M.Map String Int
+findAllOccurences _ Leaf = M.empty
+findAllOccurences cword (Node b) = M.unionsWith (+) $ (M.fromListWith (+) parovi):(map (uncurry findAllOccurences) (map (\(lab, st) -> (cword ++ lab, st)) b)) where parovi = map (\(label, subtree) -> let currWord = cword ++ label in (currWord, cntL subtree)) b
+
+
